c6e349b864bcd44a12662da528b5c91f800a5583
mapserver2007 <mapserver2007@gmail.com>
2014-09-06 23:13:53 +0900

    Postgres追加、認証処理を追加

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ var wsServer = new WebSocketServer({
     server: httpServer
 });
 
+// WebSocket Connection
 var connections = [];
 wsServer.on('connection', function(ws) {
     connections.push(ws);
@@ -40,17 +41,52 @@ wsServer.on('connection', function(ws) {
     });
 });
 
+// WebSocket send
 var broadcast = function(message) {
     connections.forEach(function (connection, index) {
         connection.send(message);
     });
 };
 
+var authenticated = {};
 app.get("/auth", function(req, res) {
+    var authKey = req.query.authKey;
 
-    console.log(req.query.authKey);
+    if (authenticated[authKey]) {
+        console.log("already authenticated");
+        res.status(200).end();
+        return;
+    }
 
-    res.status(200).end();
+    // PostgreSQL
+    var pg = require('pg');
+    var conString = process.env.DATABASE_URL;
+    pg.connect(conString, function(err, client, done) {
+        var sql = "SELECT UserID FROM M_Auth WHERE AuthKey = $1";
+        var bind = [authKey];
+
+        // DB接続失敗
+        if (err) {
+            res.status(500).end();
+            console.log("database error");
+        }
+
+        client.query(sql, bind, function(err, result) {
+            done();
+            if (err) {
+                res.status(403).end();
+                console.log("authenticate ng");
+            }
+            if (result.rows.length === 0) {
+                console.log("authenticate ng");
+                res.status(403).end();
+            } else {
+                authenticated[authKey] = result.rows[0].userid;
+                console.log("authenticate ok");
+                res.status(200).end();
+            }
+        });
+    });
 });
 
 
