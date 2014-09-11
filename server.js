21c619d6b0548bc14215432ca19a512dc7654736
mapserver2007 <mapserver2007@gmail.com>
2014-09-12 00:47:22 +0900

    Android端末とブラウザ間の通信処理を実装

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ var WebSocketServer = require('ws').Server,
     http = require('http'),
     express = require('express'),
     app = express(),
+    pg = require('pg'),
     port = process.env.PORT || 9224;
 
 app.use(express.static(__dirname + '/'));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 
 // HTTP Server
@@ -30,11 +31,50 @@ var wsServer = new WebSocketServer({
     server: httpServer
 });
 
+// load authkey list
 var authenticated = {};
+
+// PostgreSQL
+var conString = process.env.DATABASE_URL;
+pg.connect(conString, function(err, client, done) {
+    var closeServer = function(err) {
+        console.log(err);
+        httpServer.close();
+    };
+
+    // DB接続失敗
+    if (err) {
+        closeServer(err);
+        return;
+    }
+
+    var sql = "SELECT UserID, AuthKey FROM M_Auth";
+    client.query(sql, "", function(err, result) {
+        done();
+        if (err) {
+            closeServer(err);
+            return;
+        }
+
+        for (var i = 0, len = result.rows.length; i < len; i++) {
+            authenticated[result.rows[i].authkey] = result.rows[i].userid;
+        }
+
+        console.log(authenticated);
+    });
+});
+
 
 // WebSocket Connection
 var connections = [];
 wsServer.on('connection', function(ws) {
+    var authKey = ws.upgradeReq.headers['x-imadoko-authkey'];
+    // Android側からの接続は認証情報を格納する
+    // 認証情報が取れない場合は、ブラウザからの閲覧専用ユーザとする
+    if (authKey) {
+        ws._authKey = authKey;
+        ws._userId = authenticated[authKey];
+    }
     connections.push(ws);
 
     var close = function() {
@@ -44,9 +84,35 @@ wsServer.on('connection', function(ws) {
         });
     };
 
-    ws.on('message', function(authKey) {
-        if (!authenticated[authKey]) {
-            close();
+    ws.on('message', function(data) {
+        var json = JSON.parse(data);
+        // polling_from_android
+        // Androidからのポーリングリクエストは認証キーが必須
+        if (json.status === 'polling_from_android') {
+            if (!authenticated[json.authKey]) {
+                close();
+            }
+            console.log("polling request from android");
+        }
+        // polling
+        // 閲覧ユーザ(ブラウザ)からのポーリングリクエストは認証キーを必要としない
+        else if (json.status === 'polling') {
+            console.log("polling request");
+        } else if (json.status === 'getPosition') {
+            // TODO 認証キーからどのクライアントに送るかを判断する必要がある
+            // 全員の位置を≈更新したい場合のみbroadcastを使う
+            var userId = json.userId;
+            if (userId) {
+                connections.forEach(function(connection) {
+                    if (connection._userId === userId) {
+                        // 位置情報取得リクエストをAndroid端末に送信
+
+                        // TODO
+                        connection.send("hogehoge");
+                        return;
+                    }
+                });
+            }
         }
     });
 
@@ -64,61 +130,10 @@ app.get("/auth", function(req, res) {
     var authKey = req.query.authKey;
 
     if (authenticated[authKey]) {
-        console.log("already authenticated");
+        console.log("authenticate ok");
         res.status(200).end();
-        return;
-    }
-
-    // PostgreSQL
-    var pg = require('pg');
-    // var conString = process.env.DATABASE_URL;
-    var conString = process.env.DATABASE_URL;
-    pg.connect(conString, function(err, client, done) {
-        var sql = "SELECT UserID FROM M_Auth WHERE AuthKey = $1";
-        var bind = [authKey];
-
-        // DB接続失敗
-        if (err) {
-            res.status(500).end();
-            console.log("database error");
-        }
-
-        client.query(sql, bind, function(err, result) {
-            done();
-            if (err) {
-                res.status(403).end();
-                console.log("authenticate ng");
-            }
-            if (result.rows.length === 0) {
-                console.log("authenticate ng");
-                res.status(403).end();
-            } else {
-                authenticated[authKey] = result.rows[0].userid;
-                console.log("authenticate ok");
-                res.status(200).end();
-            }
-        });
-    });
-});
-
-app.post('/rest/location', function(req, res) {
-    var data = {
-        lng: req.body.lng,
-        lat: req.body.lat
-    };
-
-    try {
-        broadcast(JSON.stringify(data));
-        res.status(200).end();
-    }
-    catch (e) {
-        if (/Cannot call method 'send'/.test(e.message)) {
-            console.error("WebSocket connection is required from client.");
-            res.status(404).end();
-        }
-        else {
-            console.error(e.stack);
-            throw e;
-        }
+    } else {
+        console.log("authenticate ng");
+        res.status(403).end();
     }
 });
